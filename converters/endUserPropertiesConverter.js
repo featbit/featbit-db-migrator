3db55ffe7fc04553a8a54cf1c321acb213e59be1
deleteLater <mikcczhang@gmail.com>
2026-01-14 23:18:16 +0800

    more fix

--- converters/endUserPropertiesConverter.js
+++ converters/endUserPropertiesConverter.js
@@ -4,6 +4,21 @@ export class EndUserPropertiesConverter extends ConverterBase {
     pgColumns = ["id", "env_id", "name", "preset_values", "use_preset_values_only", "is_built_in", "is_digest_field", "remark", "created_at", "updated_at"];
 
     toCsv(mongoDocs) {
-        return super.toCsv(mongoDocs, this.pgColumns);
+      return mongoDocs.map(doc => {
+        return this.pgColumns.map(col => {
+          const mongoField = this.getMongoFieldFromPgColumn(col);
+          let value = doc[mongoField];
+
+          if (col === 'preset_values') {
+            // replace all _id with id in JSON string
+            const normalizedJsonValue = JSON.stringify(value).replace(/"_id":/g, '"id":');
+            const normalizedValue = JSON.parse(normalizedJsonValue);
+
+            return this.toStringValue(normalizedValue);
+          }
+
+          return this.toStringValue(value);
+        }).join(this.DELIMITER);
+      }).join('\n');
     }
 }
